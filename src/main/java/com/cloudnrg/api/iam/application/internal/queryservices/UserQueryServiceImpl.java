ed910f6e6532cc19510317f3a19213f9f0e21d47
Al3xiel <gabriel280104casas@gmail.com>
2025-06-18 23:58:03 -0500

    feat(user): add query handler for retrieving user by username

--- src/main/java/com/cloudnrg/api/iam/application/internal/queryservices/UserQueryServiceImpl.java
+++ src/main/java/com/cloudnrg/api/iam/application/internal/queryservices/UserQueryServiceImpl.java
@@ -2,6 +2,7 @@ package com.cloudnrg.api.iam.application.internal.queryservices;
 
 import com.cloudnrg.api.iam.domain.model.aggregates.User;
 import com.cloudnrg.api.iam.domain.model.queries.GetUserByIdQuery;
+import com.cloudnrg.api.iam.domain.model.queries.GetUserByUsernameQuery;
 import com.cloudnrg.api.iam.domain.services.UserQueryService;
 import com.cloudnrg.api.iam.infrastructure.persistance.jpa.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,10 @@ public class UserQueryServiceImpl implements UserQueryService {
     @Override
     public Optional<User> handle(GetUserByIdQuery query) {
         return userRepository.findUserById(query.userId());
+    }
+
+    @Override
+    public Optional<User> handle(GetUserByUsernameQuery query) {
+        return userRepository.findByUsername(query.username());
     }
 }
