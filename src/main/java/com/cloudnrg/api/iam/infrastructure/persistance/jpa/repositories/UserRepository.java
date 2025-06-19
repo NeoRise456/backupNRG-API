2746830048641a424e72e4c901182822ff469f14
Al3xiel <gabriel280104casas@gmail.com>
2025-06-18 22:36:24 -0500

    feat(repositories): add RoleRepository for role operations and extend UserRepository with username methods

--- src/main/java/com/cloudnrg/api/iam/infrastructure/persistance/jpa/repositories/UserRepository.java
+++ src/main/java/com/cloudnrg/api/iam/infrastructure/persistance/jpa/repositories/UserRepository.java
@@ -7,7 +7,31 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * This interface is responsible for providing the User entity related operations.
+ * It extends the JpaRepository interface.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    /**
+     * This method is responsible for finding the user by id.
+     * @param id The user id.
+     * @return The user object.
+     */
     Optional<User> findUserById(UUID id);
+
+    /**
+     * This method is responsible for finding the user by username.
+     * @param username The username.
+     * @return The user object.
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * This method is responsible for checking if the user exists by username.
+     * @param username The username.
+     * @return True if the user exists, false otherwise.
+     */
+    boolean existsByUsername(String username);
 }
