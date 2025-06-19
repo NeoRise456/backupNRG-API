62a723197166488d7146da230bad4818d8333bb1
Al3xiel <gabriel280104casas@gmail.com>
2025-06-18 22:32:51 -0500

    feat(users): add SignInCommand and SignUpCommand handling to UserCommandService, and add GetUserByUsernameQuery to UserQueryService

--- src/main/java/com/cloudnrg/api/iam/domain/services/UserCommandService.java
+++ src/main/java/com/cloudnrg/api/iam/domain/services/UserCommandService.java
@@ -2,9 +2,15 @@ package com.cloudnrg.api.iam.domain.services;
 
 import com.cloudnrg.api.iam.domain.model.aggregates.User;
 import com.cloudnrg.api.iam.domain.model.commands.CreateUserCommand;
+import com.cloudnrg.api.iam.domain.model.commands.SignInCommand;
+import com.cloudnrg.api.iam.domain.model.commands.SignUpCommand;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.Optional;
 
 public interface UserCommandService {
+    Optional<ImmutablePair<User, String>> handle(SignInCommand command);
+    Optional<User> handle(SignUpCommand command);
+    //TO-DO: Eliminar de todas las implementaciones este command y reemplazarlo por SignUpCommand
     Optional<User> handle(CreateUserCommand command);
 }
--- src/main/java/com/cloudnrg/api/iam/domain/services/UserQueryService.java
+++ src/main/java/com/cloudnrg/api/iam/domain/services/UserQueryService.java
@@ -2,9 +2,11 @@ package com.cloudnrg.api.iam.domain.services;
 
 import com.cloudnrg.api.iam.domain.model.aggregates.User;
 import com.cloudnrg.api.iam.domain.model.queries.GetUserByIdQuery;
+import com.cloudnrg.api.iam.domain.model.queries.GetUserByUsernameQuery;
 
 import java.util.Optional;
 
 public interface UserQueryService {
     Optional<User> handle(GetUserByIdQuery query);
+    Optional<User> handle(GetUserByUsernameQuery query);
 }
